b3f3b37934b4474bf93f84591d7ac66700ff6398
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-09-14 15:11:56 +0500

    класс FilmService

--- src/main/java/ru/yandex/practicum/filmorate/exception/ClassExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ClassExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -43,13 +42,5 @@ public class ClassExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("error", ex.getMessage()));
     }
-
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> exception(Exception ex) {
-        log.error("неожиданное иссключение: {}", ex.getMessage(), ex);
-        return Map.of("error", "Произошла непредвиденная ошибка");
-    }
-
 }
 
