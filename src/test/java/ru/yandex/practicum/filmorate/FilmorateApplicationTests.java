62b330240711b1dbede4ad7aff5b934086bf040e
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-09-14 14:42:46 +0500

    класс тест

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-class FilmControllerUpdateTests {
+public class FilmorateApplicationTests {
 
     @Autowired
     private FilmController filmController;
