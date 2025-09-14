3328446586ae481df56b132c3ff07ff2797fb6f3
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-09-14 14:59:59 +0500

    класс FilmService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
@@ -44,10 +43,10 @@ public class FilmService {
 
     public Film createFilm(Film film) {
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза должна быть не раньше 28.12.1895");
+            throw new RuntimeException("Дата релиза должна быть не раньше 28.12.1895");
         }
         if (film.getDuration() < 0) {
-            throw new IllegalArgumentException("Длительность должна быть положительной");
+            throw new RuntimeException("Длительность должна быть положительной");
         }
         return filmStorage.addFilm(film);
     }
