3fc61f49853f246094530b8ecf5d986559bdd423
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-09-22 18:05:21 +0500

    Убрала не нужную библиотеку

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/DirectorDbStorage.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dal.mappers.FilmRowMapper;
-import ru.yandex.practicum.filmorate.dal.mappers.FoundFilmRepository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
