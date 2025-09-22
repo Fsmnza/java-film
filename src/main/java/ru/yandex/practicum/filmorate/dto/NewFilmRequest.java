25d912e5e8b8dcb4abe7986e16c321ec4d4d9f90
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-09-22 18:04:02 +0500

    Убрала не нужную библиотеку

--- src/main/java/ru/yandex/practicum/filmorate/dto/NewFilmRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/NewFilmRequest.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.ReleaseDate;
 import ru.yandex.practicum.filmorate.model.User;
 
